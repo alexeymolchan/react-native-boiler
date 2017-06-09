30ea54dd71d9d07cab473031a85a8fbeca4bf72f
Alexey Molchan <alexeymolchan228@gmail.com>
2017-06-09 17:17:17 +0300

    fixed store Provider error

--- App.js
+++ App.js
@@ -3,17 +3,14 @@ import configureStore from './src/store/store.js';
 const {Provider} = require('react-redux');
 import Root from './src/components/Root.js';
 
-const store = () => {
-  console.log("store init");
-  return configureStore()
-};
+const store = configureStore();
 
 export default class App extends React.Component {
 
   render() {
 
     return (
-      <Provider store={store()}>
+      <Provider store={store}>
         <Root />
       </Provider>
     );
